9c005492c5bc7be6b832576fabac0bd198c00ec4
francesco-guttuso <francescoguttuso@gmail.com>
2026-05-18 01:00:58 +0200

    refactor isPalindroma function to improve readability and ensure correct palindrome check

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function isPalindroma (parola) {
+    //trasformo la Prima lettera se scritta maiuscola in minuscola
     const parolaMinuscola = parola.toLowerCase();
+    //con split divido la parola in lettere,con reverse la inverto, con join la trasforma in "parola"
     const parolaRibaltata = parolaMinuscola.split("").reverse().join("");
-    parolaMinuscola = parolaRibaltata;  
+    //se le due parole coincidono allora true
+     return parolaMinuscola === parolaRibaltata; 
+     
 }
+console.log (isPalindroma ("Casa"));
+
