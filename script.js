458eed599d7fd29a8d00bda3a061c1b699a59a0c
francescoguttuso <francescoguttuso@gmail.com>
2026-05-18 09:41:02 +0200

    refactor user number input and isEvenOrOdd function for improved validation and functionality

--- script.js
+++ script.js
@@ -12,24 +12,33 @@ if (parola) {
     console.log(`É Palindroma: ${isPalindroma(parola)}`);
 }
 
+//---esercizio Pari o Dispari---
+//chiedo all'utente di inserire un numero da 1 a 5
 
-
-
-const userNumber = 3;
+const userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
 
 if (userNumber < 1 || userNumber > 5) {
-    console.error("Non è un numero valido");
+    alert("Non è un numero valido");
 } else {
     console.log("Numero accettato!");
 }
-
+//---creo la funzione che genera un numero da 1 a 5 e dice se è pari o dispari---
 function isEvenOrOdd(number) {
     number = Math.floor(Math.random() * 6) + 1;
-    return;
+    return number % 2 === 0 ? "pari" : "dispari";
 }
-console.log(isEvenOrOdd);
+
 
 function somma(a, b) {
     const sommaNumber = a + b;
     return sommaNumber;
 }
+
+
+const risultato = somma(userNumber, isEvenOrOdd);
+if (risultato % 2 === 0) {
+    console.log("Hai vinto! Il risultato è pari");
+} else {
+    console.log("Hai perso! Il risultato è dispari");
+}
+
