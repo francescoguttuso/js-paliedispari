4bcb45de8aead976b81e1eb09fd0f85f3190ef94
francescoguttuso <francescoguttuso@gmail.com>
2026-05-18 09:10:40 +0200

    refactor isPalindroma and isEvenOrOdd functions for consistency and readability

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-function isPalindroma (parola) {
+function isPalindroma(parola) {
     //trasformo la Prima lettera se scritta maiuscola in minuscola
     const parolaMinuscola = parola.toLowerCase();
     //con split divido la parola in lettere,con reverse la inverto, con join la trasforma in "parola"
     const parolaRibaltata = parolaMinuscola.split("").reverse().join("");
     //se le due parole coincidono allora true
-     return parolaMinuscola === parolaRibaltata; 
-     
+    return parolaMinuscola === parolaRibaltata;
+
 }
-console.log (isPalindroma ("Casa"));
+let parola = prompt("Inserisci una parola");
+if (parola) {
+    console.log(`É Palindroma: ${isPalindroma(parola)}`);
+}
 
 
-const userNumber = 3; 
+
+
+const userNumber = 3;
 
 if (userNumber < 1 || userNumber > 5) {
     console.error("Non è un numero valido");
 } else {
     console.log("Numero accettato!");
-}  
-
-function isEvenOrOdd (number) {
-   number = Math.floor(Math.random() * 6) + 1;
-   return;
 }
-console.log (isEvenOrOdd);
 
-function somma (a, b) {
+function isEvenOrOdd(number) {
+    number = Math.floor(Math.random() * 6) + 1;
+    return;
+}
+console.log(isEvenOrOdd);
+
+function somma(a, b) {
     const sommaNumber = a + b;
     return sommaNumber;
 }
